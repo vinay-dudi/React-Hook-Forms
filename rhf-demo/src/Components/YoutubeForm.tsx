6b52cdc94bbc10dbf1ccf41211df2ce9dc60631c
unknown <vinay.dudi@divami.com>
2024-03-01 11:41:01 +0530

    understanding dev tools

--- rhf-demo/src/Components/YoutubeForm.tsx
+++ rhf-demo/src/Components/YoutubeForm.tsx
@@ -1,19 +1,23 @@
 import { useForm } from "react-hook-form";
+import {DevTool} from "@hookform/devtools";
 //npm i react-hook-form
+//npm i -D @hookform/devtools
 const YoutubeForm = () => {
   const form = useForm();
-  const { register } = form;
-  const { name, ref, onChange, onBlur } = register("username");
+  const { register,control } = form;
   return (
+    <>
     <form>
       <label htmlFor="username">UserName</label>
-      <input type="text" id="username" name={name} ref={ref} onBlur={onBlur} onChange={onChange}/>
+      <input type="text" id="username"  {...register("userName")}/>
       <label htmlFor="email">E Mail</label>
       <input type="text" id="email" {...register("email")} />
       <label htmlFor="channel">Channel</label>
       <input type="text" id="channel"{...register("channel")} />
       <button>Submit</button>
     </form>
+    <DevTool control={control}/>
+    </>
   );
 };
 
