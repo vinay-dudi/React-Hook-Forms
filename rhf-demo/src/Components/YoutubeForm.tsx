3fc42bb027370fa1af2a4865da97a9478b38b656
unknown <vinay.dudi@divami.com>
2024-03-01 11:18:01 +0530

    two different ways of using register

--- rhf-demo/src/Components/YoutubeForm.tsx
+++ rhf-demo/src/Components/YoutubeForm.tsx
@@ -1,18 +1,20 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from "react-hook-form";
 //npm i react-hook-form
 const YoutubeForm = () => {
-  const form =useForm();
+  const form = useForm();
+  const { register } = form;
+  const { name, ref, onChange, onBlur } = register("username");
   return (
     <form>
       <label htmlFor="username">UserName</label>
-      <input type="text" id="username" name="username" />
+      <input type="text" id="username" name={name} ref={ref} onBlur={onBlur} onChange={onChange}/>
       <label htmlFor="email">E Mail</label>
-      <input type="text" id="email" name="email" />
+      <input type="text" id="email" {...register("email")} />
       <label htmlFor="channel">Channel</label>
-      <input type="text" id="channel" name="channel" />
+      <input type="text" id="channel"{...register("channel")} />
       <button>Submit</button>
     </form>
-  )
-}
+  );
+};
 
 export default YoutubeForm;
